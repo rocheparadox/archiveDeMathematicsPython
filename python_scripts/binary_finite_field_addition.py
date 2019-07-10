af179dd278210f80c7facb2c32b1ef45ca2c964b
Roche Periyanayagam <rocheinside@gmail.com>
2019-07-10 09:04:12 +0530

    removal of directory

--- python_scripts/binary_finite_field_addition.py
+++
@@ -1,9 +0,0 @@
-field_length = int(input("Enter the length of the field: "))
-a = input("Enter first binary value: ")
-b = input("Enter second binary value: ")
-
-a = int((a[:field_length]),2)
-b = int((b[:field_length]),2)
-
-
-print(a ^ b, bin(a^b))
